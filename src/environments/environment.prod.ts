7e5249ae3c5bc82e4dd03be6b3d1e04f0907d157
Vander <vanderzago@gmail.com>
2018-03-04 13:38:13 -0300

    deploy no heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com'
+  apiUrl: 'https://algamoney-api-vz.herokuapp.com'
+ // apiUrl: 'http://localhost:8080'
 };
